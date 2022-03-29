f7033f744de2d19d539b2407c97586f9476a2c54
Luis Diego Viquez Espinoza <ldve@hotmail.com>
2022-03-29 15:44:20 -0600

    Add project files.

---
+++ AirTiquicia.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AirTiquicia.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ AirTiquicia.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AirTiquicia.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
